a6916c98120248163e5fb77871c0d9d560d2f997
moumitazaman <moumitasub@gmail.com>
2021-11-07 16:56:00 +0600

    Frontend changed to old version

--- resources/views/layouts/frontend/partials/head.blade.php
+++ resources/views/layouts/frontend/partials/head.blade.php
@@ -1,53 +1,26 @@
-<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="csrf-token" content="{{ csrf_token() }}">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title> @stack('title') - {{ config('app.name') }} </title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="csrf-token" content="{{ csrf_token() }}">
 <meta name="description" content="{{ config('app.name') }}">
 <meta name="author" content="{{ config('app.name') }}">
 <meta name="keywords" content="">
 <meta name="robots" content="*">
+<link rel="icon" href="#" type="image/x-icon">
+<link rel="shortcut icon" href="#" type="image/x-icon">
 
-	<!-- TITLE -->
-	<title>@stack('title') - {{ config('app.name') }} </title>
-
-	<!-- FONT-ICONS -->
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/lib/fontawesome/web-fonts-with-css/css/fontawesome-all.min.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/lib/Icon-font-7-stroke-PIXEDEN/css/pe-icon-7-stroke.css') }}">
-	<link rel="stylesheet" type="text/css" href="lib/et-line-font/style.css') }}">
-
-	<!-- LIB -->
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/lib/animation/animate.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/lib/sweetalert/sweetalert.css') }}">
-
-	<!-- EFFECT -->
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/lib/vegas/vegas.min.css') }}">
-	<link rel="stylesheet" type="text/css" href="lib/jquery.mb.YTPlayer-master/jquery.mb.YTPlayer.min.css') }}">
-
-	<!-- TEMPLATE -->
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/lib/fullPage.js-master/dist/jquery.fullpage.min.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/lib/bootstrap/css/bootstrap.min.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/css/main.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/css/ckav-grids.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/css/helper.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/css/responsive.css') }}">
-
-	<!-- THEME -->
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/css/themes/default.css') }}">
-
-	<!-- CUSTOM -->
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/css/custom.css') }}">
-
-	<!-- FAVICONS -->
-	<link rel="icon" href="images/favicons/favicon.ico">
-	<link rel="icon apple-touch-icon" type="image/png" href="{{ asset('assets/frontend/images/favicons/apple-icon.png') }}">
-	<link rel="icon apple-touch-icon" type="image/png" sizes="72x72" href="{{ asset('assets/frontend/images/favicons/apple-icon-72x72.png') }}">
-	<link rel="icon apple-touch-icon" type="image/png" sizes="114x114" href="{{ asset('assets/frontend/images/favicons/apple-icon-114x114.png') }}">
-
-	{{-- <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-79547588-3"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-79547588-3');
-</script> --}}
+<!-- CSS Style -->
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/stylesheet/bootstrap.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/stylesheet/font-awesome.css') }}" media="all">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/stylesheet/bootstrap-select.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/stylesheet/revslider.css') }}" >
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/stylesheet/owl.carousel.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/stylesheet/owl.theme.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/stylesheet/jquery.bxslider.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/stylesheet/jquery.mobile-menu.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/stylesheet/style.css') }}" media="all">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/stylesheet/responsive.css') }}" media="all">
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,700italic,400,600,700,800' rel='stylesheet' type='text/css'>
+<link href="https://fonts.googleapis.com/css?family=Teko:300,400,500,600,700" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Saira+Condensed:300,400,500,600,700,800" rel="stylesheet">
+@stack('head')
