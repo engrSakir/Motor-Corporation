8dfb521791dc051454b0ec98d5d7d6cb13b5d92c
Md. Sakir Ahmed <52326976+mdsakirahmed@users.noreply.github.com>
2021-11-22 17:02:25 +0600

    Dashboard

--- resources/views/backend/dashboard/index.blade.php
+++ resources/views/backend/dashboard/index.blade.php
@@ -8,7 +8,7 @@
         <div class="d-flex justify-content-end align-items-center">
             <ol class="breadcrumb justify-content-end">
                 <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
-                <li class="breadcrumb-item active">Dashboard/li>
+                <li class="breadcrumb-item active">Dashboard</li>
             </ol>
             <a href="{{ url('/') }}" target="_blank" class="btn btn-info d-none d-lg-block m-l-15"><i class="fa fa-plus-circle"></i> Website </a>
         </div>
