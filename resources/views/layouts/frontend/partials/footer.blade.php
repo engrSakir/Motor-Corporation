4e754b165baccac014b7536daedc019121b3186d
Nahim Udoy <67038120+NahimBinRezaUdoy@users.noreply.github.com>
2022-01-16 14:35:43 +0600

    Social Icon Added

--- resources/views/layouts/frontend/partials/footer.blade.php
+++ resources/views/layouts/frontend/partials/footer.blade.php
@@ -2,31 +2,35 @@
     <!-- BEGIN INFORMATIVE FOOTER -->
     <div class="footer-inner">
         {{-- <div class="newsletter-row">
-        <div class="container">
-          <div class="row">
+            <div class="container">
+                <div class="row">
 
-            <!-- Footer Newsletter -->
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 col1">
-              <div class="newsletter-wrap">
-                <h5>Newsletter</h5>
-                <h4>Get Notified Of any Updates!</h4>
-                <form action="#" method="post" id="newsletter-validate-detail1">
-                  <div id="container_form_news">
-                    <div id="container_form_news2">
-                      <input type="text" name="email" id="newsletter1" title="Sign up for our newsletter" class="input-text required-entry validate-email" placeholder="Enter your email address">
-                      <button type="submit" title="Subscribe" class="button subscribe"><span>Subscribe</span></button>
+                    <!-- Footer Newsletter -->
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 col1">
+                        <div class="newsletter-wrap">
+                            <h5>Newsletter</h5>
+                            <h4>Get Notified Of any Updates!</h4>
+                            <form action="#" method="post" id="newsletter-validate-detail1">
+                                <div id="container_form_news">
+                                    <div id="container_form_news2">
+                                        <input type="text" name="email" id="newsletter1"
+                                            title="Sign up for our newsletter"
+                                            class="input-text required-entry validate-email"
+                                            placeholder="Enter your email address">
+                                        <button type="submit" title="Subscribe"
+                                            class="button subscribe"><span>Subscribe</span></button>
+                                    </div>
+                                    <!--container_form_news2-->
+                                </div>
+                                <!--container_form_news-->
+                            </form>
+                        </div>
+                        <!--newsletter-wrap-->
                     </div>
-                    <!--container_form_news2-->
-                  </div>
-                  <!--container_form_news-->
-                </form>
-              </div>
-              <!--newsletter-wrap-->
+                </div>
             </div>
-          </div>
-        </div>
-        <!--footer-column-last-->
-      </div> --}}
+            <!--footer-column-last-->
+        </div> --}}
         <div class="container">
             <div class="row">
                 <div class="col-sm-4 col-xs-12 col-lg-4">
@@ -44,8 +48,7 @@
                     <div class="footer-column">
                         <h4>Quick Links</h4>
                         <ul class="links">
-                            <li class="first"><a title="How to buy"
-                                    href="http://themesground.com/blog/">Blog</a></li>
+                            <li class="first"><a title="How to buy" href="http://themesground.com/blog/">Blog</a></li>
                             <li><a title="FAQs" href="#">FAQs</a></li>
                             <li><a title="Payment" href="#">Payment</a></li>
                             <li><a title="Shipment" href="#">Shipment</a></li>
@@ -96,17 +99,23 @@
                 <div class="col-sm-4 col-xs-12" style="text-align: center;">
                     <div class="social">
                         <ul>
-                            <li class="fb"><a href="https://www.facebook.com/MotorCorporationBD" target="_blank"></a></li>
+                            <li class="fb"><a href="https://www.facebook.com/MotorCorporationBD" target="_blank"></a>
+                            </li>
+                            <li class="ins"><a href="#" target="_blank"></a>
+                            <li class="youtube"><a href="#" target="_blank"></a>
+                            </li>
                             <!-- <li class="tw"><a href="#"></a></li>
                             <li class="googleplus"><a href="#"></a></li>
                             <li class="rss"><a href="#"></a></li>
                             <li class="pintrest"><a href="#"></a></li>
                             <li class="linkedin"><a href="#"></a></li>-->
-                            <li class="instagram"><a href="https://www.instagram.com/motor_corporation/" target="_blank"></a></li>
+                            <li class="instagram"><a href="https://www.instagram.com/motor_corporation/"
+                                    target="_blank"></a></li>
                         </ul>
                     </div>
                 </div>
-                <div class="col-sm-4 col-xs-12 coppyright">&copy; Developed by <a href="https://iciclecorporation.com/" target="_blank">Icicle Corporation</a>. </div>
+                <div class="col-sm-4 col-xs-12 coppyright">&copy; Developed by <a href="https://iciclecorporation.com/"
+                        target="_blank">Icicle Corporation</a>. </div>
             </div>
         </div>
     </div>
